12a991eb6a01d5a5f26fd8cd50a71010beab8567
michelle.ashby <michelle.ashby@brentwood.bc.ca>
2017-08-14 16:01:00 -0700

    working on button functions in controller and added new model status

---
+++ app/Status.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michelle.ashby
+ * Date: 8/14/17
+ * Time: 3:56 PM
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+class Status extends Model
+{
+
+    // This class is for statuses
+    // Button have one status
+    /**
+     * @var string
+     */
+    protected $table = 'status';
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'status_id';
+
+    /**
+     * Creates a hasMany relationship between student->buttons
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function button(){
+        return $this->hasMany('App\Button', 'button_id');
+    }
+}
