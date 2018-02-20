c897f583225bd25dc94588214f01ee497f54c163
michelle.ashby <michelle.ashby@brentwood.bc.ca>
2018-02-19 20:59:55 -0800

    tryingto move getStudent function to studentController

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -23,33 +23,33 @@ class PagesController extends Controller
 //            (new Student)->getStudents();
 
 
-//            //NOTE: for reference
-//            // custom_field_8 is "Student Type",
-//            // custom_field_13 is "Discount Value CAD",
-//            // custom_field_1 is "Data Validation Complete",
-//            // custom_field_9 is "Deposit Received",
-//            // custom_field_2 is "Enrollment Status"
-//            $students = $student->join('class_students', 'contacts.user_id', '=', 'class_students.user_id')
-//            ->join('classes', 'class_students.class_id', '=', 'classes.class_id')
-//            ->join('class_levels', 'classes.class_level_id', '=', 'class_levels.class_level_id')
-//            ->join('students', 'contacts.user_id', '=', 'students.user_id')
-//            ->select('contacts.user_id',
-//            'contacts.surname',
-//            'contacts.name',
-//            'students.custom_field_8',
-//            'students.custom_field_13',
-//            'students.custom_field_1',
-//            'students.custom_field_9',
-//            'students.custom_field_2')
-//            ->where('classes.year', '=', 2018)
-//            //->where('students.custom_field_1', '=', 'Yes')
-//            //->where('students.custom_field_2', '=', 'Attending 2017-2018')
-//            //->where('students.custom_field_9', '=', 'Yes')
-//            //->orderby('class_levels.class_level_index')
-//            ->get();
-////            dd($students);
+            //NOTE: for reference
+            // custom_field_8 is "Student Type",
+            // custom_field_13 is "Discount Value CAD",
+            // custom_field_1 is "Data Validation Complete",
+            // custom_field_9 is "Deposit Received",
+            // custom_field_2 is "Enrollment Status"
+            $students = $student->join('class_students', 'contacts.user_id', '=', 'class_students.user_id')
+            ->join('classes', 'class_students.class_id', '=', 'classes.class_id')
+            ->join('class_levels', 'classes.class_level_id', '=', 'class_levels.class_level_id')
+            ->join('students', 'contacts.user_id', '=', 'students.user_id')
+            ->select('contacts.user_id',
+            'contacts.surname',
+            'contacts.name',
+            'students.custom_field_8',
+            'students.custom_field_13',
+            'students.custom_field_1',
+            'students.custom_field_9',
+            'students.custom_field_2')
+            ->where('classes.year', '=', 2018)
+            //->where('students.custom_field_1', '=', 'Yes')
+            //->where('students.custom_field_2', '=', 'Attending 2017-2018')
+            //->where('students.custom_field_9', '=', 'Yes')
+            //->orderby('class_levels.class_level_index')
+            ->get();
+//            dd($students);
 
-            $students = Student::getStudents();
+//            $students = Student::getStudents();
             return view('home')->with('students', $students); //->with('buttons', $buttons);
         } else {
             return view('welcome');
