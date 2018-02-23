33b38de7867711958a0589d87ba5150db0ad21fd
michelle.ashby <michelle.ashby@brentwood.bc.ca>
2018-02-22 16:46:57 -0800

    trouble shooting search function

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -66,9 +66,9 @@ class StudentController extends Controller
                     'students.custom_field_1',
                     'students.custom_field_9',
                     'students.custom_field_2')
-                ->where('classes.year', '=', 2017)
-                ->where('contacts.name', 'LIKE', "%{searchInput}%")
-                ->orWhere('contacts.surname', 'LIKE', "%{searchInput}%")
+                ->where('classes.year', '=', 2018)
+                ->where('contacts.name', 'LIKE', $searchInput)
+                ->orWhere('contacts.surname', 'LIKE', $searchInput)
                 ->get();
 
             if (count($students) == 0) {
