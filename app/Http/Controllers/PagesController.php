2b71fc93ffee11790b8e40d7e9601bafb24c0540
michelle.ashby <michelle.ashby@brentwood.bc.ca>
2018-03-01 14:09:59 -0800

    paginate to 10

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -43,7 +43,7 @@ class PagesController extends Controller
             //->where('students.custom_field_9', '=', 'Yes')
             //->orderby('class_levels.class_level_index')
 //            ->get();
-            ->paginate(20);
+            ->paginate(10);
 //            dd($students);
 
             $studentCount = $student->join('class_students', 'contacts.user_id', '=', 'class_students.user_id')
