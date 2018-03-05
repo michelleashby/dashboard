c64b85ee1ad271c5f51d18e92316bc56118ef067
michelle.ashby <michelle.ashby@brentwood.bc.ca>
2018-03-05 15:34:13 -0800

    paginate to 20

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -24,27 +24,28 @@ class PagesController extends Controller
 
             $student = new Student();
 
-            $students = $student->join('class_students', 'contacts.user_id', '=', 'class_students.user_id')
-            ->join('classes', 'class_students.class_id', '=', 'classes.class_id')
-            ->join('class_levels', 'classes.class_level_id', '=', 'class_levels.class_level_id')
-            ->join('students', 'contacts.user_id', '=', 'students.user_id')
-            ->select('contacts.user_id',
-                'contacts.surname',
-                'contacts.name',
-                'students.custom_field_8',  // custom_field_8 is "Student Type"
-                'students.custom_field_13', // custom_field_13 is "Discount Value CAD"
-                'students.custom_field_1',  // custom_field_1 is "Data Validation Complete"
-                'students.custom_field_9', // custom_field_9 is "Deposit Received"
-                'students.custom_field_2') // custom_field_2 is "Enrollment Status"
-            ->where('classes.year', '=', 2018)
-            ->orderby('contacts.surname')
-            //->where('students.custom_field_1', '=', 'Yes')
-            //->where('students.custom_field_2', '=', 'Attending 2017-2018')
-            //->where('students.custom_field_9', '=', 'Yes')
-            //->orderby('class_levels.class_level_index')
-//            ->get();
-            ->paginate(20);
-//            dd($students);
+            $students = $student->getStutents();
+//              $student->join('class_students', 'contacts.user_id', '=', 'class_students.user_id')
+//            ->join('classes', 'class_students.class_id', '=', 'classes.class_id')
+//            ->join('class_levels', 'classes.class_level_id', '=', 'class_levels.class_level_id')
+//            ->join('students', 'contacts.user_id', '=', 'students.user_id')
+//            ->select('contacts.user_id',
+//                'contacts.surname',
+//                'contacts.name',
+//                'students.custom_field_8',  // custom_field_8 is "Student Type"
+//                'students.custom_field_13', // custom_field_13 is "Discount Value CAD"
+//                'students.custom_field_1',  // custom_field_1 is "Data Validation Complete"
+//                'students.custom_field_9', // custom_field_9 is "Deposit Received"
+//                'students.custom_field_2') // custom_field_2 is "Enrollment Status"
+//            ->where('classes.year', '=', 2018)
+//            ->orderby('contacts.surname')
+//            //->where('students.custom_field_1', '=', 'Yes')
+//            //->where('students.custom_field_2', '=', 'Attending 2017-2018')
+//            //->where('students.custom_field_9', '=', 'Yes')
+//            //->orderby('class_levels.class_level_index')
+////            ->get();
+//            ->paginate(20);
+////            dd($students);
 
             $studentCount = $student->join('class_students', 'contacts.user_id', '=', 'class_students.user_id')
                 ->join('classes', 'class_students.class_id', '=', 'classes.class_id')
