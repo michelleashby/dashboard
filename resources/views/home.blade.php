d5eb02f9584d8b9d0003a1e50660a4466bd3beb1
michelle.ashby <michelle.ashby@brentwood.bc.ca>
2018-03-09 10:06:21 -0800

    trying new approach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,26 +35,25 @@
 
                 <td>{{$student->name}} {{$student->surname}} <br> {{$student->user_id}}</td>
 
+                {{$studentButtons = $student->button()->orderby('step_id', 'ASC')->get()}}
 
 
-            {{--@if($buttons != null)--}}
-                    {{--@foreach($buttons as $button)--}}
-                        {{--@if($button->student_id == $student->user_id)--}}
-                        {{--<td>--}}
-                            {{--<button type="button" class="{{$button->button_class}}" style="white-space: normal">{{$button->button_words}}</button>--}}
-                        {{--</td>--}}
-                        {{--@endif--}}
-                    {{--@endforeach--}}
-                {{--@endif--}}
+            @if($studentButtons != null)
+                    @foreach($studentButtons as $button)
+                        <td>
+                            <button type="button" class="{{$button->button_class}}" style="white-space: normal">{{$button->button_words}}</button>
+                        </td>
+                    @endforeach
+                @endif
 
                 {{--<td> --}}
                 {{--data validation--}}
                     {{--@if($student->custom_field_1 = "Yes")--}}
                         {{--@if($button->step_id = 1)--}}
-                {{$valButton = $student->button()->where('step_id',1)->get()}}
-                        <td>
-                        <button type="button" class="{{$valButton->button_class}}" style="white-space: normal">{{$valButton->button_words}}</button>
-                        </td>
+                {{--{{$valButton = $student->button()->where('step_id',1)->get()}}--}}
+                        {{--<td>--}}
+                        {{--<button type="button" class="{{$valButton->button_class}}" style="white-space: normal">{{$valButton->button_words}}</button>--}}
+                        {{--</td>--}}
                         {{--@endif--}}
                         {{--<button type="button" class="btn btn-success disabled" style="white-space: normal">Validation Complete--}}
                             {{--<span class="glyphicon glyphicon-ok"></span>--}}
