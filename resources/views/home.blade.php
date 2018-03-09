cbb27f7d8202562fef7f026f25d3a6450df697f2
michelle.ashby <michelle.ashby@brentwood.bc.ca>
2018-03-09 13:03:17 -0800

    trying buttons a new way again

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -50,8 +50,8 @@
                     {{--@if($student->custom_field_1 = "Yes")--}}
                         {{--@if($student->button()->step_id = 1)--}}
                         <td>
-                        <button type="button" class="{{$student->button()->where('step_id',1)->first()->button_class}}" style="white-space: normal">
-                            {{$student->button()->where('step_id',1)->first()->button_words}}</button>
+                        <button type="button" class="{{$student->button()->where('step_id',1)->get()->button_class}}" style="white-space: normal">
+                            {{$student->button()->where('step_id',1)->get()->button_words}}</button>
                         </td>
                         {{--@endif--}}
                         {{--<button type="button" class="btn btn-success disabled" style="white-space: normal">Validation Complete--}}
