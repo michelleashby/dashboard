8285702c2fe73aa1c529832b358da20c2c917c5e
michelle.ashby <michelle.ashby@brentwood.bc.ca>
2018-03-09 10:33:12 -0800

    trying new approach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,23 +39,22 @@
 
 
             @if($studentButtons != null)
-                    @foreach($studentButtons as $button)
-                        <td>
-                            <button type="button" class="{{$button->button_class}}" style="white-space: normal">{{$button->button_words}}</button>
-                        </td>
-                    @endforeach
-                @endif
+                    {{--@foreach($studentButtons as $button)--}}
+                        {{--<td>--}}
+                            {{--<button type="button" class="{{$button->button_class}}" style="white-space: normal">{{$button->button_words}}</button>--}}
+                        {{--</td>--}}
+                    {{--@endforeach--}}
+                {{--@endif--}}
 
-                {{--<td>--}}
+
                 {{--data validation--}}
                     {{--@if($student->custom_field_1 = "Yes")--}}
-                        {{--@if($button->step_id = 1)--}}
-                {{--{{$valButton = $student->button()->where('step_id',1)->get()}}--}}
-                        {{--<td>--}}
-                        {{--<button type="button" class="{{$student->button()->where('step_id',1)->get()->button_class}}" style="white-space: normal">--}}
-                            {{--{{$student->button()->where('step_id',1)->get()->button_words}}</button>--}}
-                        {{--</td>--}}
-                        {{--@endif--}}
+                        @if($button->step_id = 1)
+                        <td>
+                        <button type="button" class="{{$button->button_class}}" style="white-space: normal">
+                            {{$button->button_words}}</button>
+                        </td>
+                        @endif
                         {{--<button type="button" class="btn btn-success disabled" style="white-space: normal">Validation Complete--}}
                             {{--<span class="glyphicon glyphicon-ok"></span>--}}
                         {{--</button> </td>--}}
@@ -150,6 +149,8 @@
 
 
                         {{--@endforeach--}}
+                    @endif
+
             </tr>
         @endforeach
         </tbody>
