c32343cae02fb46ea41becc920303a2c44fa90d8
michelle.ashby <michelle.ashby@brentwood.bc.ca>
2018-03-09 15:28:55 -0800

    adding dbdate to sync

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,7 @@
                                     </li>
                                     <li>
                                         <a href="/dbsync">Re-Sync DB</a>
-                                        Last Sync: {{$dbdate}}
+                                        Last Sync: {{$dbDate}}
                                     </li>
                                     <li>
                                         <a href="{{ route('logout') }}"
