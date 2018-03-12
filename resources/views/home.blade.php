08e9712137db0e0ac5a2da8be9a4e1be938c6853
michelle.ashby <michelle.ashby@brentwood.bc.ca>
2018-03-12 11:49:15 -0700

    trying to get buttons next to the students that they belong to

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -178,12 +178,12 @@
                         {{--<button type="button" class="btn disabled" style="white-space: normal">Resend Head Prefect Email</button> </td>--}}
                     {{--@endif--}}
                 </td>
+                        @endif
 
 
                         @endforeach
 
             </tr>
-                @endif
         @endforeach
         </tbody>
     </table>
