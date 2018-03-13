3c4b653bb21a669b63949feafeff00bd670d979f
michelle.ashby <michelle.ashby@brentwood.bc.ca>
2018-03-13 12:57:10 -0700

    debugging

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -29,7 +29,7 @@ class PagesController extends Controller
             $studentCount = $student->all()->count();
 
             $dbSync = DB::connection('mysql')->select('select DATE(updated_at) from db_sync order by updated_at DESC limit 1');
-//            dd($dbSync);
+            dd($dbSync);
 
             return view('home')->with('students', $students)->with('studentCount', $studentCount)->with('dbDate', $dbSync);
         } else {
