ac646bd7102fe9e32be76653ab95a363ab9ec63a
michelle.ashby <michelle.ashby@brentwood.bc.ca>
2018-03-13 13:30:33 -0700

    fixing routing error

--- app/Http/Controllers/ButtonController.php
+++ app/Http/Controllers/ButtonController.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Input;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
 
 class ButtonController extends Controller
 {
