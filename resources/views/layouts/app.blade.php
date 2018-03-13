58227445f46f65699f8c0acc5dea752bad617109
michelle.ashby <michelle.ashby@brentwood.bc.ca>
2018-03-13 11:47:40 -0700

    debugging

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,7 @@
                                     </li>
                                     <li>
                                         <a href="/dbsync">Re-Sync DB</a>
-                                        <h8>Last Sync: {{ $dbDate->updated_at}}<h8>
+                                        <h8>Last Sync: {{ var_dump($dbDate)}}<h8>
                                     </li>
                                     <li>
                                         <a href="{{ route('logout') }}"
